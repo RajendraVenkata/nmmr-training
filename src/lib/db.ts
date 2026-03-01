219e3678dd02ed30d2734238f220260a8b57e5b8
Palem Venkata Rajendra <palem@macbookpro.local>
2026-03-01 18:09:39 +0530

    project complated 1

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -30,7 +30,9 @@ export async function connectDB() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "nmmr-training",
+    }).then((m) => m);
   }
 
   cached.conn = await cached.promise;
